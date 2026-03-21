3af015ca9eb807ae15cdc435cb747c29f18f3a1b
ifeday1 <oladeniifeoluwa123@gmail.com>
2026-03-21 22:07:14 +0100

    pod update

--- src/data/podcasts.js
+++ src/data/podcasts.js
@@ -4,18 +4,19 @@ import Podthumb2 from '../assets/podthumb2.jpeg';
 const podcasts = [
   {
     title:
-      'Faith, Creativity & Cultural Influence:Becoming a Kingdom-Minded Creative in Today"s Media Landscape',
-    thumbnail: Podthumb1,
-    youtube: 'https://youtu.be/OgkiwkmdsZ8?si=MqV4Ho_edcGvIgYl',
-    date: '27th,January 2026',
-  },
-  {
-    title:
       'Engineering, Excellence, and Evolving with Grace: Leading in Industry and the Home.',
     thumbnail: Podthumb2,
     youtube: 'https://youtu.be/yKRczvfaxZM?si=hVNsnTn-dFkECspW',
     date: '18th,March 2026',
   },
+  {
+    title:
+      'Faith, Creativity & Cultural Influence:Becoming a Kingdom-Minded Creative in Today"s Media Landscape',
+    thumbnail: Podthumb1,
+    youtube: 'https://youtu.be/OgkiwkmdsZ8?si=MqV4Ho_edcGvIgYl',
+    date: '27th,January 2026',
+  },
+
   // {
   //   title: 'Leadership & Personal Growth',
   //   thumbnail: '/podcasts/podcast3.jpg',
